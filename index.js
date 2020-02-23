a52176da1a337185256139bedb2eb1f1ac363358
Luminita Manolache <luminitalumi.manolache@gmail.com>
2020-02-23 09:16:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var result = [];
+  for(var i = 0; i < musicians.length; i++) {
+    var str = musicians[i] + " plays " + instruments[i];
+    result.push(str);
+  }
+  return result;
+}
+
+function johnLennonFacts(facts) {
+  var johnFacts = [];
+  let i = 0;
+
+  while(i < facts) {
+
+      johnFacts.push(i);
+      i++;
+  }
+   return johnFacts + "!!!";
+}
