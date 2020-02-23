ed695a6dd362fcf2b1fd29cd54f9c5de7abdb77a
Luminita Manolache <luminitalumi.manolache@gmail.com>
2020-02-23 11:27:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,8 +25,9 @@ function iLoveTheBeatles(num) {
   var resArray = [];
   var i = 0;
 
+
   do {
-    result += num[i] + "I love the Beatles!";
+  var result = num[i] + "I love the Beatles!";
     resArray.push(result);
     i++;
   }
